c34be8a13cdd6039a2ffacc8b1fa4c66a647842a
shyazusa <shy.azusa@gmail.com>
2016-06-18 10:12:21 +0900

    First commit

---
+++ app.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require 'uri'
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+require 'unicode'
+
+URL = 'https://ja.wikipedia.org/wiki/'
+
+get '/search' do
+  keyword = URI.escape(params['keyword'].gsub("\s", '_'))
+  ACCESS_URL = "#{URL}#{keyword}"
+  title = get_title
+  table = make_table.gsub("\n", '<br>')
+  "#{title}<br>#{ACCESS_URL}<br><br>#{table}"
+end
+
+helpers do
+  def access_wiki
+    html = open(ACCESS_URL).read
+    Nokogiri::HTML.parse(html)
+  end
+
+  def get_title
+    access_wiki.title
+  end
+
+  def make_table
+    doc = access_wiki
+    table = doc.xpath('//table[@class="infobox bordered"]')
+    echo = "|||\n|:---:|:---:|"
+    table.css('tr').each do |tr|
+      th = tr.css('th').text
+      td = tr.css('td').text
+      echo = "#{echo}\n|#{th}|#{td}|" unless td == ''
+    end
+    echo
+  end
+end
